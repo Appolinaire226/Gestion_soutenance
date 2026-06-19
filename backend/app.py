8f369f1fb696f36618c12a752f0f1371236d9b24
Appolinaire226 <appolinaireadouabou1@gmail.com>
2026-06-19 05:25:05 +0000

    rectification du requirements

--- backend/app.py
+++ backend/app.py
@@ -36,12 +36,13 @@ def create_app():
 
     @app.route("/")
     def index():
-        return {"message": "API Gestion Soutenances — OK ✓"}
+        return {"message": "Bienvenue sur l'API de gestion des soutenances!"}
 
     return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)  
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
                          
