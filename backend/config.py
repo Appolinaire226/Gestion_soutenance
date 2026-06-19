aeaa4e25e4a2af21a3e6ba92e31303040945d05c
Appolinaire226 <appolinaireadouabou1@gmail.com>
2026-06-19 04:39:55 +0000

    preparation deploiement Render

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-import secrets
-
-class Config:
-    SECRET_KEY = secrets.token_hex(32)
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/jurys_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
